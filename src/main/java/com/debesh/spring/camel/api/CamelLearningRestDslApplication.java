03bbcd0ab1d7d741aeb8408df18b6d28e98bae1f
Debesh Nayak <dnayak382@gmail.com>
2021-12-28 13:46:07 +0530

    inital commit

---
+++ src/main/java/com/debesh/spring/camel/api/CamelLearningRestDslApplication.java
@@ -0,0 +1,13 @@
+package com.debesh.spring.camel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelLearningRestDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelLearningRestDslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debesh/spring/camel/api/CamelLearningRestDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debesh.spring.camel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelLearningRestDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
