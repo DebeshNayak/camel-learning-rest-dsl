ecd577cd0f75392b9260bab8cc436faa1aadc855
Debesh Nayak <dnayak382@gmail.com>
2021-12-29 19:13:52 +0530

    added post endpoint

---
+++ src/main/java/com/debesh/spring/camel/api/processor/OrderProcessor.java
@@ -0,0 +1,21 @@
+package com.debesh.spring.camel.api.processor;
+
+import com.debesh.spring.camel.api.dto.Order;
+import com.debesh.spring.camel.api.service.OrderService;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderProcessor implements Processor {
+
+    @Autowired
+    private OrderService service;
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        service.addOrder(exchange.getIn().getBody(Order.class));
+    }
+
+}
--- src/main/java/com/debesh/spring/camel/api/resource/ApplicationResource.java
+++ src/main/java/com/debesh/spring/camel/api/resource/ApplicationResource.java
@@ -1,6 +1,9 @@
 package com.debesh.spring.camel.api.resource;
 
+import com.debesh.spring.camel.api.dto.Order;
+import com.debesh.spring.camel.api.processor.OrderProcessor;
 import com.debesh.spring.camel.api.service.OrderService;
+import org.apache.camel.BeanInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,9 @@ public class ApplicationResource extends RouteBuilder {
     @Autowired
     private OrderService service;
 
+    @BeanInject
+    private OrderProcessor processor;
+
     @Override
     public void configure() throws Exception {
         restConfiguration().component("servlet").port(9090).host("localhost").bindingMode(RestBindingMode.json);
@@ -21,13 +27,25 @@ public class ApplicationResource extends RouteBuilder {
                 .get("/hello-world")
                 .produces(MediaType.APPLICATION_JSON_VALUE)
                 .route()
-                .setBody(constant("Welcome to camel rest api"));
+                .setBody(constant("Welcome to camel rest api"))
+                .endRest();
 
         rest()
                 .get("/getOrders")
                 .produces(MediaType.APPLICATION_JSON_VALUE)
                 .route()
-                .setBody(() -> service.getOrders());
+                .setBody(() -> service.getOrders())
+                .endRest();
+
+        rest()
+                .post("/addOrder")
+                .consumes(MediaType.APPLICATION_JSON_VALUE)
+                .produces(MediaType.APPLICATION_JSON_VALUE)
+                .type(Order.class)
+                .outType(Order.class)
+                .route()
+                .process(processor)
+                .endRest();
 
     }
 
